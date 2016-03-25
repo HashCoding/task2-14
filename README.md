e31103353db1a1c7877e4418e6ee297a3124c3c1
rccoder <rccoder.net@gmail.com>
2016-03-25 13:36:18 +0800

    optimization :dizzy:

---
+++ README.md
@@ -0,0 +1,3 @@
+# 任务十四：零基础JavaScript编码 (二)
+
+[Document.createDocumentFragment() 创建文件片段](http://ife.baidu.com/note/detail?noteId=513)
--- app.js
+++ app.js
@@ -9,22 +9,28 @@ var aqiData = [
 
 (function () {
 
+  var sorted_data = aqiData.filter(function(data) {
+    return data[1] > 60;
+  }).sort(function(n1, n2) {
+    return n1 < n2;
+  });
+
   var aqi_list = document.getElementById("aqi-list");
  
-  for(var i = 0, l = aqiData.length; i < l; i++) {
 
-    if(aqiData[i][1] > 60) {
+  var _li_fragments = document.createDocumentFragment();
+
+  for(var i = 0, l = sorted_data.length; i < l; i++) {
 
       var _li = document.createElement("li");
-      var _data_content = '城市:' + aqiData[i][0] + ',空气质量' + aqiData[i][1];
+      var _data_content = '城市:' + sorted_data[i][0] + ',空气质量' + sorted_data[i][1];
       var _li_content = document.createTextNode(_data_content);
 
       _li.appendChild(_li_content);
       
-      aqi_list.appendChild(_li);  
-      
-    }
-
+      _li_fragments.appendChild(_li);  
   }
+
+  aqi_list.appendChild(_li_fragments);
 
 })();
